7092d20c8e000441e37ccd670a830526b23823c8
Anton Erdman <aerdman@iba.by>
2018-08-20 15:28:34 +0300

    Playing with sounds and sequences.

--- scheme.scm
+++ scheme.scm
@@ -1,2 +1,30 @@
+(load "lib.scm")
+
+;; square wave function (sort-of)
+(define (sqr freq t) (if (> 0 (sin (* t freq 2 pi))) 1 0))
+(define (rnd) (- (random 2.) 1))
+
+(define (my-sound f t0)
+  (let ((freq f)
+        (l (line-down t0 0.3)))
+    (lambda (t)
+      (+ (* (sqr freq t) 0.5)
+         (* (rnd) (* (l t) 0.5)) ))))
+
+(define sequencer
+  (make-loop-seq :A4 :E4 :G4 :D4 :F4 :C4 :E4 :B3))
+
+(define t1 0)
+(define dt 0.7)   
+(define cur-freq 0)
+
+(define my-snd (my-sound 0 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define (f t in)
-  (* (sin (* t 220 2 pi)) 0.9))
+  (if (>= t (+ t1 dt))
+    (begin
+      (set! my-snd (my-sound (note->freq (sequencer)) t))
+      (set! t1 t)))
+
+  (my-snd t))
